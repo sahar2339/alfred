dcbd9d55ad1bfa867e2bae19d021e9d56a7b7ab8
itay bachmannn <itayb1010@gmail.com>
2023-03-26 11:58:44 +0300

    changed stuff

---
+++ src/models/pod.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class UnhealthyPod(BaseModel):
+    pass
+
+
+class Pod(BaseModel):
+    pass
---
+++ src/utils/factory.py
@@ -0,0 +1,6 @@
+def get_resolver_factory(name: str):
+    pass
+
+
+def get_kube_client_factory(name: str):
+    pass
